d2be448f6308cd62b95294b231508eb914a63630
rulymauzi-byte <rulymauzi@gmail.com>
2026-09-08 09:10:49 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MAUZI_CONFIG = {
   appName: 'Mauzi Música',
-  apiUrl: 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwEhznzVneSxMjVH5iPT41Jfw-0ztkz3T7mwqV9PAD8Kb-kcFHdHAVEE0wkUFbUhWzv/exec',
   rememberLastFolder: true,
   autoLoadLastFolder: true,
   theme: 'dark'
